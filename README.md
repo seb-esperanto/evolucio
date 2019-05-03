4c25c3b6b8a5dc803d1fde9462bef6bf08163cf6
Paul Würtz <paulwuertz@posteo.de>
2019-05-03 11:14:54 +0200

    * unu ludebla kartaro

---
+++ README.md
@@ -0,0 +1,25 @@
+# Evolucio
+
+Tio estas traduko de la kartludo evolucio.
+
+![](img/repoimg.png)
+
+Bonvole subtenu la originalan ludon kaj ĝuu ĝin nun ankaŭ kun viaj esperantaj ludemulaj amikojn ;)
+
+Dankon al wikimedia por la libraj bildoj por la kartoj.
+
+## Krei la kartaron
+
+```
+python genkartoj.py
+```
+
+Kaj poste kompili en LaTeX. Mi uzis texlive sub Manjaro Linux.
+
+## Versioj
+
+### 1.0
+
+* kompleta kartaro
+* rektangulaj kartoj
+* 9 kartoj po paĝo
---
+++ genkartoj.py
@@ -0,0 +1,37 @@
+import json, math, random
+
+pagxekajxo = """\n\n \\pagebreak \n\n"""
+pagxinterajxo = """\n\n\n"""
+
+karto = """
+        \\begin{{tikzpicture}}
+        \\cardbackground{{{0}}}{{{1}}}
+        \\cardtitle{{{2}}}
+        \\cardcontent{{{3}}}
+        \\cardborder  {4}
+        \\end{{tikzpicture}}"""
+
+dekstra = "\n\\fill (5.7,.33) -- (5.7,2.33) -- (6,1.33) -- cycle;"
+maldekstra = "\\fill (.4,.33) -- (.4,2.33) -- (.1,1.33) -- cycle;\n"
+
+def makeKart(jsEl):
+    image, bgcolor, title, cont = jsEl["img"], jsEl["koloro"], \
+                                  jsEl["title"], jsEl["teksto"]
+    arrows = ""
+    if jsEl["dekstra"]    : arrows += dekstra
+    if jsEl["maldekstra"] : arrows += maldekstra
+    return karto.format(image, bgcolor, title, cont, arrows)
+
+cards = open("kartaro.json").read()
+kartoj = json.loads(cards)
+ludo = ". \n\n \\vspace{.95cm}\n\n"
+c=0
+for kart in kartoj:
+    for j in range(kart["kvanto"]):
+        if c%9==0 and c!=0:
+            ludo += pagxekajxo
+        elif c%3==0 or c%6==0 and c!=0:
+            ludo += pagxinterajxo
+        ludo += makeKart(kart)
+        c+=1
+open("kartaro.tex","w").write(ludo)
